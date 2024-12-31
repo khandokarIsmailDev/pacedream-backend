c9555b42e081866aa6877d8255121c92afadef58
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2025-01-01 03:13:59 +0600

    add listen for running port

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -46,13 +46,13 @@ app.use((err:any,_req:any,res:any,_next:any) =>{
 
 
 // Remove or comment out this part for Vercel deployment
-// const port = process.env.PORT || 4003
-// const serviceName = process.env.SERVICE_NAME || 'user-service'
+const port = process.env.PORT || 4003
+const serviceName = process.env.SERVICE_NAME || 'user-service'
 
 
-// app.listen(port,() =>{
-//     console.log(`${serviceName} running on port ${port}`)
-// })
+app.listen(port,() =>{
+    console.log(`${serviceName} running on port ${port}`)
+})
 
 //for vercel production
 export default app
