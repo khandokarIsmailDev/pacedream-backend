f99536260fac567c117eb7af666a929fb657f613
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-12-31 23:20:38 +0600

    vercel production bug fix

--- auth/src/controllers/userRegistration.ts
+++ auth/src/controllers/userRegistration.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import prisma from "../prisma";
 import { UserCreateSchema } from "../schemas";
+import { AccountStatus } from "@prisma/client";
 
 const userRegistration = async (
   req: Request,
@@ -28,16 +29,17 @@ const userRegistration = async (
     const userData = {
       ...parsedBody.data,
       auth0Id: parsedBody.data.auth0Id || "",
+      status: parsedBody.data.status as AccountStatus | undefined,
     };
 
     const user = await prisma.user.create({
       data: userData,
     });
     res.status(201).json({
-        message: "user-created-successfully",
-        data: user
-      });
-      return
+      message: "user-created-successfully",
+      data: user,
+    });
+    return;
   } catch (error) {
     console.log(error);
     next(error);
